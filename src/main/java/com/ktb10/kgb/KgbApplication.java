6dd05e708b7e6c39201c13ebb3ffe508d161f682
DDONG <74868332+shet6006@users.noreply.github.com>
2026-09-15 20:58:57 +0900

    chore: Spring Boot 프로젝트 초기 환경 구성 (#8)

---
+++ src/main/java/com/ktb10/kgb/KgbApplication.java
@@ -0,0 +1,12 @@
+package com.ktb10.kgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KgbApplication.class, args);
+    }
+}
---
+++ src/test/java/com/ktb10/kgb/KgbApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ktb10.kgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KgbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
