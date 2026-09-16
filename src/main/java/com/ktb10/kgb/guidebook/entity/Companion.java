fe5345c29395a658086b88baa8dad141c2ff5786
DDONG <shet6006@gmail.com>
2026-09-16 20:02:56 +0900

    feat: 가이드북 도메인 Enum 추가

---
+++ src/main/java/com/ktb10/kgb/guidebook/entity/Companion.java
@@ -0,0 +1,9 @@
+package com.ktb10.kgb.guidebook.entity;
+
+public enum Companion {
+    ALONE,
+    FRIEND,
+    COUPLE,
+    FAMILY,
+    GROUP
+}
---
+++ src/main/java/com/ktb10/kgb/guidebook/entity/GenerationStatus.java
@@ -0,0 +1,9 @@
+package com.ktb10.kgb.guidebook.entity;
+
+public enum GenerationStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED,
+    CANCELED
+}
