a4932991a78496e4386c0962f8b182ffa092bfe5
DDONG <shet6006@gmail.com>
2026-09-16 15:37:40 +0900

    chore: MySQL 및 JPA Flyway 개발 기반 구성

--- src/test/java/com/ktb10/kgb/KgbApplicationTests.java
+++ src/test/java/com/ktb10/kgb/KgbApplicationTests.java
@@ -2,8 +2,10 @@ package com.ktb10.kgb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KgbApplicationTests {
 
     @Test
