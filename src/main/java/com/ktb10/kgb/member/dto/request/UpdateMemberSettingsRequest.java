28acb760075805091f14a53e02cdc71be957d520
Yongjun Kim <78668991+Yonduss@users.noreply.github.com>
2026-09-23 16:51:19 +0900

    feat(member): 회원 설정 요청과 응답 계약 구현

---
+++ src/main/java/com/ktb10/kgb/member/dto/request/UpdateMemberSettingsRequest.java
@@ -0,0 +1,13 @@
+package com.ktb10.kgb.member.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import jakarta.validation.constraints.NotNull;
+
+/** 회원의 V1 푸시 수신 설정을 변경하는 요청입니다. */
+@JsonDeserialize(using = UpdateMemberSettingsRequestDeserializer.class)
+public record UpdateMemberSettingsRequest(
+        @JsonProperty("push_enabled")
+        @NotNull
+        Boolean pushEnabled) {
+}
